45811cfe7fd70ae0ee24a507d3ecabcbe6f920db
Anderson <andemartinezdev@gmail.com>
2020-09-30 21:55:40 -0500

    Clase 5-7 -> Pedir carta
    - Se creo una función el cual retorna una carta y la elimina del deck.

--- assets/js/juego.js
+++ assets/js/juego.js
@@ -3,6 +3,7 @@ let deck        = [];
 const tipos     = ['C', 'D', 'H', 'S'];
 const specials  = ['A', 'J', 'Q', 'K'];
 
+//Funcion para crear baraja
 const crearDeck = () => {
 
     for(let i = 2; i < 11; i++) {
@@ -16,9 +17,30 @@ const crearDeck = () => {
             deck.push(special + tipo);
         }
     }
-    
+
+    deck = _.shuffle(deck);
+
     return deck;
+}
+
+//Esta funcion me permite tomar una carta
+const pedirCarta = () => {
+    
+    let carta = '';
+
+    if(deck.length >= 1){
+        // carta = deck.pop();
+        carta = deck.splice(0, 1);
+    }
+    else{
+        throw 'No hay cartas en el deck';
+    }
+    
+    return carta;
 }
 
 
 crearDeck();
+console.log(deck);
+console.log(pedirCarta());
+console.log(deck);
