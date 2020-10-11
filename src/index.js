b6e8e2029799c9e9ce9c5d5d1445a7ee795327af
Anderson <andemartinezdev@gmail.com>
2020-10-11 15:28:17 -0500

    Clase 10-6 -> TodoList class
    - Se creo la clase que manejara las tareas.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import './styles.css';
+import {Todo, TodoList} from './classes';
 
-import { Todo } from './classes/todo.class';
+// Para evitar las lineas de importacion, se centraliza en index.js
+// import { Todo } from './classes/todo.class';
+// import { TodoList } from './classes/todo-list.class';
 
-
+const todoList = new TodoList();
 const tarea = new Todo('aprender js');
+const tarea2 = new Todo('andemar');
 
-console.log(tarea);
+todoList.nuevoTodo(tarea);
+todoList.nuevoTodo(tarea2);
+
+console.log(todoList);
