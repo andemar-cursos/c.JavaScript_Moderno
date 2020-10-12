8cd9ead9e6369e2cdeac0600dee2e9ae6c37c83a
Anderson <andemartinezdev@gmail.com>
2020-10-12 12:46:35 -0500

    Clase 11-12 -> Await
    - Se creo una función el cual utiliza await para la espera de data.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,5 @@
+import { obtenerHeroesArr } from './js/await';
 import './styles.css';
 
-import { buscarHeroe, buscarHeroeAsync } from './js/promesas'
 
-buscarHeroe('capi2')
-    .then(console.log)
-    .catch(console.warn);
-
-
-buscarHeroeAsync('iron2')
-    .then(console.log)
-    .catch(console.warn);
-
+obtenerHeroesArr().then(console.table);
