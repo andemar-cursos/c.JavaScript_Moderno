1c3f8593e755cd6d3be29331fb8bf74f5146bcfb
Anderson <andemartinezdev@gmail.com>
2020-10-12 12:19:26 -0500

    Clase 11-10 -> Promise Race
    - Funcion que ejecuta X cantidad de promesas, pero solo resuelve la primera que llegue con data/err.

--- src/index.js
+++ src/index.js
@@ -1,2 +1,12 @@
 import './styles.css';
 
+import { promesaLenta, promesaMedia, promesaRapida } from './js/promesas'
+
+// promesaLenta.then(console.log);
+// promesaMedia.then(console.log);
+// promesaRapida.then(console.log);
+
+// Se resuelve la priemra promesa que haya sido resulta
+Promise.race([promesaLenta, promesaMedia, promesaRapida])
+       .then(console.log)
+       .catch(console.warn);
