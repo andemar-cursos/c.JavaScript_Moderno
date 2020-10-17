c50d98818826c92f844b0fd42fd5adbd56a01e4f
Anderson <andemartinezdev@gmail.com>
2020-10-17 16:56:49 -0500

    Clase 12-4 -> Peticion Fetch
    - Se hizo una funcion con peticion Fetch

--- src/index.js
+++ src/index.js
@@ -1,4 +1,11 @@
 
-const nombre = 'andemar';
+const jokeUrl = `https://api.chucknorris.io/jokes/random`;
 
-saludar(nombre);
+fetch( jokeUrl).then( resp => {
+    
+    resp.json().then( ({ id, value}) => {
+        console.log(id);
+        console.log(value);
+    });
+
+})
