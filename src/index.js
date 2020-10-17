7528b8cbd6e864f962f357eaa5347eced20b2e81
Anderson <andemartinezdev@gmail.com>
2020-10-17 17:15:18 -0500

    Clase 12-6 -> Fetch async/await
    - Se hizo una peticion Fetch con el uso de async y await.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,4 @@
+const { obtenerChiste } = require("./js/http-provider");
 
-const jokeUrl = `https://api.chucknorris.io/jokes/random`;
 
-fetch( jokeUrl).then( resp => {
-    
-    resp.json().then( ({ id, value}) => {
-        console.log(id);
-        console.log(value);
-    });
-
-})
+obtenerChiste().then(console.log);
---
+++ src/js/http-provider.js
@@ -0,0 +1,37 @@
+import { async } from "q";
+
+const jokeUrl = `https://api.chucknorris.io/jokes/random`;
+
+
+const obtenerChiste = async() => {
+
+    try {
+        const resp = await fetch(jokeUrl);
+    
+        if( !resp.ok) throw 'No se pudo realizar la petcion';
+    
+        const { icon_url, id, value} = await resp.json();
+
+        return {icon_url, id, value};
+
+    } catch (error) {
+        throw error;
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+export {
+    obtenerChiste,
+}
